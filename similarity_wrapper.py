e8523854943fcac18ae9158420803a52766e334c
xhinliang <xhinliang@gmail.com>
2024-06-29 17:55:56 +0800

    Init

--- similarity_wrapper.py
+++ similarity_wrapper.py
@@ -1,7 +1,9 @@
 from image_similarity_measures.evaluate import evaluation
-import cv2
 
 def similarity(a, b, metric: str):
     return evaluation(org_img_path=a, 
            pred_img_path=b, 
            metrics=[metric])[metric]
+
+def similarity_wrapper(a, b, metric):
+    return similarity(a, b, metric)
